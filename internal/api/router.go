7647ce7174f107ce8b9d36dd48532002b0121d34
WendelHime <wendelhime@hotmail.com>
2023-08-11 21:06:41 -0300

    feat: add middlewares to router

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,18 +1,31 @@
 package api
 
 import (
+	"github.com/WendelHime/hydroponics-metrics-collector/internal/api/endpoints"
+	"github.com/WendelHime/hydroponics-metrics-collector/internal/api/middlewares"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/httplog"
 	"github.com/go-chi/render"
 	"github.com/rs/zerolog"
 )
 
-func NewRouter(logger zerolog.Logger, metricsEndpoints MetricsEndpoints, userEndpoints UserEndpoints) chi.Router {
+func NewRouter(logger zerolog.Logger, metricsEndpoints endpoints.MetricsEndpoints, userEndpoints endpoints.UserEndpoints) chi.Router {
 	mux := chi.NewRouter()
 	mux.Use(httplog.RequestLogger(logger))
 	mux.Use(render.SetContentType(render.ContentTypeJSON))
 
-	mux.Post("/metrics", metricsEndpoints.RegisterMetric)
+	// private endpoints for metrics
+	mux.Group(func(r chi.Router) {
+		r.Use(middlewares.EnsureValidToken)
+		r.Use(middlewares.HasScope("write:metrics"))
+
+		r.Post("/metrics", metricsEndpoints.RegisterMetric)
+	})
+
+	// private endpoints for binding user to sensor
+	// TODO: create endpoints for binding user to sensor
+
+	// public endpoints
 	mux.Post("/users", userEndpoints.CreateAccount)
 	mux.Post("/signin", userEndpoints.SignIn)
 
