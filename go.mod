9ddb605d3b54d74b33595c208c008c45d5422410
WendelHime <wendelhime@hotmail.com>
2023-08-15 16:01:34 -0300

    feat: add http endpoints for adding and retrieving user devices

--- go.mod
+++ go.mod
@@ -31,6 +31,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-chi/jwtauth/v5 v5.1.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
--- internal/api/endpoints/user_endpoints.go
+++ internal/api/endpoints/user_endpoints.go
@@ -8,7 +8,9 @@ import (
 	"github.com/WendelHime/hydroponics-metrics-collector/internal/logic"
 	"github.com/WendelHime/hydroponics-metrics-collector/internal/shared/errors"
 	"github.com/WendelHime/hydroponics-metrics-collector/internal/shared/models"
+	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
 	"github.com/rs/zerolog/log"
 )
 
@@ -70,6 +72,72 @@ func (e UserEndpoints) SignIn(w http.ResponseWriter, r *http.Request) {
 	}
 
 	response := LoginResponse{AccessToken: token.AccessToken}
+	render.Render(w, r, response)
+	render.Status(r, http.StatusOK)
+}
+
+type AddDeviceRequest struct {
+	Device string `json:"device" validate:"required"`
+	UserID string `validate:"required"`
+}
+
+func (a *AddDeviceRequest) Bind(r *http.Request) error {
+	validate := validator.New()
+	err := validate.Struct(a)
+	if err != nil {
+		return errors.BadRequestErr.WithErr(err).WithMsg("failed to validate request")
+	}
+	return nil
+}
+
+func (e UserEndpoints) AddDevice(w http.ResponseWriter, r *http.Request) {
+	userID := chi.URLParam(r, "userID")
+	request := AddDeviceRequest{UserID: userID}
+	err := render.Bind(r, &request)
+	if err != nil {
+		log.Warn().Err(err).Msg("failed to decode add device request")
+		errors.RenderErr(w, r, err)
+		return
+	}
+
+	err = e.logic.AddDevice(r.Context(), userID, request.Device)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to add new device")
+		errors.RenderErr(w, r, err)
+		return
+	}
+
+	render.Status(r, http.StatusAccepted)
+}
+
+type GetDevicesResponse struct {
+	UserID  string   `json:"user_id"`
+	Devices []string `json:"devices"`
+}
+
+func (g GetDevicesResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (e UserEndpoints) GetDevices(w http.ResponseWriter, r *http.Request) {
+	userID := chi.URLParam(r, "userID")
+	if len(userID) == 0 {
+		errors.RenderErr(w, r, errors.BadRequestErr.WithMsg("missing user ID"))
+		return
+	}
+
+	devices, err := e.logic.GetDevices(r.Context(), userID)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to retrieve devices")
+		errors.RenderErr(w, r, err)
+		return
+	}
+
+	response := GetDevicesResponse{
+		UserID:  userID,
+		Devices: devices,
+	}
+
 	render.Render(w, r, response)
 	render.Status(r, http.StatusOK)
 }
